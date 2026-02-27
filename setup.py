d7fcdd5a03fc7d33ccd2bc7bf95c97069323d735
LIU Yu <liuyu@huimtlab.org>
2026-02-27 20:13:29 +0800

    update spike's VERSION string before compile

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import contextlib
 import glob
 import importlib
 import os
@@ -24,6 +25,7 @@ import subprocess
 import tempfile
 from setuptools import setup
 from setuptools.command.build_py import build_py as _build_py
+from setuptools_scm import get_version, ScmVersion
 # pylint: disable=import-error
 from pybind11.setup_helpers import Pybind11Extension, build_ext as _bulid_ext
 
@@ -60,12 +62,31 @@ def _config_bridge() -> Pybind11Extension:
     )
 
 
+@contextlib.contextmanager
+def _patch_version(source_dir: pathlib.Path):
+    with source_dir.joinpath("VERSION").open("r+") as f_ver:
+        default_version = f_ver.read()
+        try:
+            semantic_version = getattr(package, "__version__", None)
+            if semantic_version is None:
+                semantic_version = get_version(local_scheme="setup:local_scheme")
+            f_ver.seek(0)
+            f_ver.write(f"#define SPIKE_VERSION \"{semantic_version}\"\n")
+            f_ver.truncate()
+            yield semantic_version
+        finally:
+            f_ver.seek(0)
+            f_ver.write(default_version)
+            f_ver.truncate()
+
+
 def _build_spike(package_dir: pathlib.Path) -> None:
     source_dir = package_dir.parent.parent.parent.parent.joinpath("vendor", "spike")
     dest_dir = package_dir.joinpath("data")
     if dest_dir.joinpath("include", "riscv", "encoding.h").exists():
         return
-    with tempfile.TemporaryDirectory(prefix="build.", dir=source_dir) as build_dir:
+    with tempfile.TemporaryDirectory(prefix="build.", dir=source_dir) as build_dir, \
+            _patch_version(source_dir):
         current_dir = os.getcwd()
         try:
             os.chdir(build_dir)
@@ -74,6 +95,18 @@ def _build_spike(package_dir: pathlib.Path) -> None:
             subprocess.run(["make", "install"], env=os.environ, check=True)
         finally:
             os.chdir(current_dir)
+
+
+def local_scheme(version: ScmVersion):
+    if version.exact or not version.distance:
+        return ""
+    spike_commit = subprocess.run([
+        "git", "rev-parse", "--short", "HEAD:vendor/spike"
+    ], check=True, capture_output=True, text=True).stdout.strip()
+    pyspike_commit = subprocess.run([
+        "git", "rev-parse", "--short", "HEAD"
+    ], check=True, capture_output=True, text=True).stdout.strip()
+    return f"+g{pyspike_commit}.spike.g{spike_commit}"
 
 
 class build_py(_build_py):
@@ -106,7 +139,7 @@ class build_ext(_bulid_ext):
         return super().build_extensions()
 
 
-setup(
-    ext_modules=[_config_bridge(), ],
-    cmdclass={"build_py": build_py, "build_ext": build_ext}
-)
+if __name__ == '__main__':
+    setup(
+        ext_modules=[_config_bridge(), ],
+        cmdclass={"build_py": build_py, "build_ext": build_ext})
